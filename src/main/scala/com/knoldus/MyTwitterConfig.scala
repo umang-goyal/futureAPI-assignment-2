89baa90de7fe9cfd988eb86a6025798479c9ddf5
umang-goyal <goel97umang@gmail.com>
2020-03-06 05:04:10 +0530

    removed twitter keys

--- src/main/scala/com/knoldus/MyTwitterConfig.scala
+++ src/main/scala/com/knoldus/MyTwitterConfig.scala
@@ -6,10 +6,6 @@ import twitter4j.auth.AccessToken
 object MyTwitterConfig extends TwitterConfigurationBuilder {
 
   val config: Config = ConfigFactory.load()
-//  val consumerKey = "e6uS4phTxImI68qlA6h4V3zwR"
-//  val consumerSecret = "M8b4Q3sudgU9mNZgJx1onUlqQYi5h5YCK1GVacjAc8yHDAohFc"
-//  val token = "160922224-AKOoOasbqi3huqT7uyq4Og0Oqlucn8rKeD9IcUvU"
-//  val tokenSecret = "7HgIJUmjOX2AZThvVp7RPWsZwOrW1ffpvkEpjeBSQynnH"
 
 
   twitter.setOAuthConsumer(config.getString("consumer.key"),
